e2bf4b22187a5b238404de5d7e332f5828002d41
Michael <mdeanjones@gmail.com>
2025-08-13 10:39:40 +0200

    add fonts

--- src/module.ts
+++ src/module.ts
@@ -1,12 +1,15 @@
-import { installModule, addComponentsDir, createResolver, defineNuxtModule, addImportsDir } from '@nuxt/kit';
+import { addComponentsDir, createResolver, defineNuxtModule } from '@nuxt/kit';
 import { importCSS } from './utils/import-css';
+import { importModules } from './utils/import-modules';
 import { importTailwind } from './utils/import-tailwind';
 
 
 export interface ModuleOptions {
-  plugins?: string[];
-  sources?: string[];
-  imports?: string[];
+  tailwind?: {
+    plugins?: string[];
+    sources?: string[];
+    imports?: string[];
+  };
 }
 
 export interface ModuleHooks {
@@ -33,15 +36,16 @@ export default defineNuxtModule<ModuleOptions>({
 
   async setup(options, nuxt) {
     await importTailwind(nuxt);
+    await importModules(nuxt);
 
     const resolver   = createResolver(import.meta.url);
     const components = resolver.resolve('./runtime/components/');
 
     addComponentsDir({ path: components });
 
-    const sources = options.sources?.slice() ?? [];
-    const plugins = options.plugins?.slice() ?? [];
-    const imports = options.plugins?.slice() ?? [];
+    const sources = options.tailwind?.sources?.slice() ?? [];
+    const plugins = options.tailwind?.plugins?.slice() ?? [];
+    const imports = options.tailwind?.plugins?.slice() ?? [];
 
     sources.push(components);
     plugins.push('@egoist/tailwindcss-icons');
@@ -50,28 +54,5 @@ export default defineNuxtModule<ModuleOptions>({
     nuxt.hook('modules:done', async () => {
       await importCSS(nuxt, { sources, plugins, imports });
     });
-
-    await installModule('@primevue/nuxt-module', {
-      autoImport : false,
-      loadStyles : false,
-
-      options: {
-        unstyled: true,
-      },
-
-      directives: {
-        prefix  : 'Prime',
-        include : [],
-      },
-
-      components: {
-        prefix  : 'Prime',
-        include : [],
-      },
-
-      composables: {
-        include: [],
-      },
-    }, nuxt);
   },
 });
---
+++ src/utils/import-modules.ts
@@ -0,0 +1,43 @@
+import { installModule, useNuxt } from '@nuxt/kit';
+import type { Nuxt } from '@nuxt/schema';
+
+
+/**
+ * Appends the config with additional modules that this one
+ * relies on.
+ */
+export async function importModules(nuxt: Nuxt = useNuxt()) {
+  await installModule('@primevue/nuxt-module', {
+    autoImport : false,
+    loadStyles : false,
+
+    options: {
+      unstyled: true,
+    },
+
+    directives: {
+      prefix  : 'Prime',
+      include : [],
+    },
+
+    components: {
+      prefix  : 'Prime',
+      include : [],
+    },
+
+    composables: {
+      include: [],
+    },
+  }, nuxt);
+
+  await installModule('@nuxt/fonts', {
+    families: [
+      {
+        name     : 'Archivo',
+        provider : 'google',
+        styles   : ['normal'],
+        weights  : [200, 300, 400, 500],
+      },
+    ],
+  });
+}
