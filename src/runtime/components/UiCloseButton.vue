fbf9c9ad406037b8a253856fb0cbf63b21663933
Michael <mdeanjones@gmail.com>
2025-10-15 09:11:41 +0200

    feat: introduce UiCloseButton for standardization of more complex components that require one

---
+++ src/runtime/components/UiCloseButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <UiButton
+    :aria-label = "primevue.config.locale?.aria?.close"
+    :tooltip    = "primevue.config.locale?.aria?.close"
+    :bordered   = "false"
+    icon        = "i-ph-x"
+  />
+</template>
+
+<script setup lang="ts">
+  import { usePrimeVue } from "primevue/config";
+  const primevue = usePrimeVue();
+</script>
