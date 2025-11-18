eacb48c82586f0da9c48e93b644f06eaf38522f7
Michael <mdeanjones@gmail.com>
2025-11-18 15:19:05 +0100

    style: updated Drawer component to play better with the page scrollbar around all four sides that it can slide in from

--- playground/pages/components/ui-drawer.vue
+++ playground/pages/components/ui-drawer.vue
@@ -3,15 +3,17 @@
 
   <UiButton text="Open" @click="showDrawer = true" />
 
+  <UiLorem type="paragraphs" min="20" />
 
-
-  <UiDrawer v-model:visible="showDrawer" header="Drawer Title" position="right">
-    Hello World
+  <UiDrawer v-model:visible="showDrawer" header="Drawer Title" position="top">
+    <UiLorem type="paragraphs" max="2" />
+    <hr class="hr">
+    <UiLorem type="paragraphs" max="10" />
   </UiDrawer>
 </template>
 
+
 <script setup lang="ts">
   import { ref } from 'vue';
-
   const showDrawer = ref(false);
 </script>
--- src/runtime/components/Drawer.vue
+++ src/runtime/components/Drawer.vue
@@ -1,27 +1,30 @@
 <template>
   <PrimeDrawer
-    :class          ="['drawer', `drawer-${ props.position ?? 'left' }`]"
-    :visible        = "props.visible"
-    :position       = "props.position"
-    :block-scroll   = "props.blockScroll"
-    :aria-label     = "props.showHeaderText ? undefined : props.header"
-    :pt             = "passthroughProps"
-    @update:visible = "updatedValue => emits('update:visible', updatedValue)"
+    :class           ="['drawer', `drawer-${ props.position ?? 'left' }`]"
+    :visible         = "props.visible"
+    :position        = "props.position"
+    :block-scroll    = "props.blockScroll"
+    :aria-label      = "props.showHeaderText ? undefined : props.header"
+    :show-close-icon = "props.showCloseIcon"
+    :pt              = "passthroughProps"
+    @update:visible  = "updatedValue => emits('update:visible', updatedValue)"
   >
-    <template #closebutton="{ closeCallback }">
-      <CloseButton @click="closeCallback" />
-    </template>
+    <template #container="{ closeCallback }">
+      <div v-if="props.showHeaderText || props.showCloseIcon" class="header">
+        <div class="title">
+          <slot v-if="props.showHeaderText" name="title">
+            {{ props.header }}
+          </slot>
+        </div>
 
-    <template #header>
-      <div class="title">
-        <slot v-if="props.showHeaderText" name="title">
-          {{ props.header }}
-        </slot>
+        <div>
+          <CloseButton @click="closeCallback" />
+        </div>
       </div>
-    </template>
 
-    <template #default>
-      <slot />
+      <div class="content">
+        <slot />
+      </div>
     </template>
   </PrimeDrawer>
 </template>
@@ -56,35 +59,17 @@
 
   const passthroughProps = computed(() => {
     return {
-      header() {
-        return {
-          class: [
-            'header',
-            {
-              'me-3': props.position !== 'right' || calculateBodyScrollbarWidth() === 0,
-            },
-          ],
-        }
+      root: () => ({
+        style: `--outgoing-scrollbar-width: ${ calculateBodyScrollbarWidth() }px;`,
+      }),
+
+      mask: {
+        class: 'occlude-background',
       },
 
-      content: {
-        class: 'content',
-      },
-
-      mask() {
-        return {
-          class: [
-            'occlude-background',
-            {
-              'replace-scrollbar': props.position === 'right',
-            },
-          ],
-        }
-      },
-
-      transition: {
+      transition: () => ({
         name: `slide-in-${ props.position ?? 'left' }`,
-      },
+      }),
     };
   });
 </script>
@@ -95,12 +80,37 @@
   @reference "tailwindcss";
 
   .drawer {
-    --drawer-border-radius: --spacing(3);
+    --outgoing-scrollbar-width : 0px;
+    --drawer-border-radius : --spacing(3);
+    --drawer-spacing       : 15px;
+    --drawer-spacing-end   : calc(var(--drawer-spacing) - var(--outgoing-scrollbar-width) + var(--p-scrollbar-width));
 
-    background   : var(--color-global-background);
-    border-color : var(--color-global-background-accent);
+    /*
+     To explain the `--drawer-spacing` variables. Primevue sets the `--p-scrollbar-width` variable to the
+     pixel width of the scrollbar whenever it disables scrolling by setting the body's overflow to hidden.
+     This value can then be used to make up space that the scrollbar occupied via whatever mechanism is desired,
+     and therefore avoid everything on the page reflowing into that newly freed up space - this is a good thing.
+
+     The only problem here is that Primevue sets that variable _after_ the open/close transitions have taken
+     place for the Drawer component, and so the fact that "there is a scrollbar now, but soon there will not be"
+     needs to be taken into account in a way that keeps the overall size and layout of the Drawer unchanged.
+     To do this, we take our own measurement of the scrollbar and use that to offset all of the content on the side
+     that the scrollbar will be disappearing from. The result is an effect that looks like the Drawer is sliding in
+     behind the scrollbar. Not perfect, but pretty decent.
+     */
+
+    background           : var(--color-global-background);
+    border-color         : var(--color-global-background-accent);
+    padding-block        : var(--drawer-spacing);
+    padding-inline-start : var(--drawer-spacing);
+    padding-inline-end   : var(--drawer-spacing-end);
+    display              : flex;
+    flex-direction       : column;
+    gap                  : var(--drawer-spacing);
 
     &.drawer-left {
+      --drawer-spacing-end: var(--drawer-spacing);
+
       border-right-width         : 1px;
       border-top-right-radius    : var(--drawer-border-radius);
       border-bottom-right-radius : var(--drawer-border-radius);
@@ -114,7 +124,7 @@
       border-top-left-radius    : var(--drawer-border-radius);
       border-bottom-left-radius : var(--drawer-border-radius);
       height                    : 100%;
-      width                     : --spacing(80);
+      width                     : calc(--spacing(80) + var(--p-scrollbar-width));
       box-shadow                : var(--shadow-heavy-left);
     }
 
@@ -123,6 +133,7 @@
       border-bottom-left-radius  : var(--drawer-border-radius);
       border-bottom-right-radius : var(--drawer-border-radius);
       width                      : 100%;
+      max-height                 : --spacing(100);
       box-shadow                 : var(--shadow-heavy);
     }
 
@@ -131,16 +142,15 @@
       border-top-left-radius  : var(--drawer-border-radius);
       border-top-right-radius : var(--drawer-border-radius);
       width                   : 100%;
+      max-height              : --spacing(100);
       box-shadow              : var(--shadow-heavy-top);
     }
   }
 
   .drawer .header {
-    display             : flex;
+    display: flex;
     align-items         : center;
-    margin-inline-start : --spacing(3);
-    margin-top          : --spacing(3);
-    padding-bottom      : --spacing(3);
+    padding-bottom      : var(--drawer-spacing);
     border-bottom       : solid 1px var(--color-global-background-accent);
 
     .title {
@@ -150,11 +160,7 @@
   }
 
   .drawer .content {
-    padding-top          : --spacing(3);
-    padding-bottom       : --spacing(3);
-    padding-inline-start : --spacing(3);
-    height               : 100%;
-    width                : 100%;
-    overflow-y           : auto;
+    flex-grow  : 1;
+    overflow-y : auto;
   }
 </style>
