61f1038ada99a0be36baba49522cc3c6e6d8e0b7
Michael <mdeanjones@gmail.com>
2025-12-15 12:29:44 +0100

    style: better resee gradient support in Tailwind

---
+++ playground/pages/gradients.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col gap-4">
+    <div>
+      <p class="p lead">Linear Gradient (to right)</p>
+      <div class="h-24 resee-bg-linear-to-r" />
+    </div>
+
+    <div>
+      <p class="p lead">Conic Gradient</p>
+      <div class="h-96 w-96 resee-bg-conic-0" />
+    </div>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+  definePageMeta({
+    heading: 'Gradients',
+  });
+</script>
