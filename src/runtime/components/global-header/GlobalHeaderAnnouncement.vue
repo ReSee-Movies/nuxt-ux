95cfcc127d382c8c66554aed4910b2676b93fdda
Michael <mdeanjones@gmail.com>
2025-12-22 14:07:13 +0100

    feat: introduce GlobalHeaderAnnouncement component

---
+++ src/runtime/components/global-header/GlobalHeaderAnnouncement.vue
@@ -0,0 +1,82 @@
+<template>
+  <div
+    v-if      = "!isDismissed"
+    :class    = "['announcement', { dismissable: props.dismissable }]"
+    role      = "alert"
+    aria-live = "polite"
+  >
+    <LayoutPageColumn :class="{ 'relative': props.dismissable }">
+      <slot />
+
+      <CloseButton
+        v-if   = "props.dismissable"
+        class  = "absolute right-3 top-1/2 -translate-y-1/2"
+        size   = "sm"
+        @click = "dismissAnnouncement()"
+      />
+    </LayoutPageColumn>
+  </div>
+</template>
+
+
+<script lang="ts">
+  export interface GlobalHeaderAnnouncementProps {
+    announcementId? : string;
+    dismissable?    : boolean;
+  }
+</script>
+
+
+<script setup lang="ts">
+  import { computed, ref } from 'vue';
+  import CloseButton from '../CloseButton.vue';
+  import LayoutPageColumn from '../LayoutPageColumn.vue';
+  import { useReseeUx } from '../../composables/use-resee-ux';
+
+  const props = withDefaults(
+    defineProps<GlobalHeaderAnnouncementProps>(),
+    {
+      announcementId : undefined,
+      dismissable    : true,
+    },
+  );
+
+  const { preferences } = useReseeUx();
+  const isLocallyDismissed = ref(false);
+
+  const isDismissed = computed(() => {
+    return isLocallyDismissed.value
+      || (props.announcementId && preferences.value.dismissNotification === props.announcementId);
+  });
+
+  function dismissAnnouncement() {
+    if (props.announcementId) {
+      preferences.value.dismissNotification = props.announcementId;
+    }
+
+    isLocallyDismissed.value = true;
+  }
+</script>
+
+
+<style scoped>
+  @reference "tailwindcss";
+
+  .announcement {
+    --announcement-bg-color: white;
+
+    @variant dark {
+      --announcement-bg-color: black;
+    }
+
+    background-image:
+      linear-gradient(to bottom, var(--announcement-bg-color) 0 calc(100% - 10px), transparent),
+      linear-gradient(to right, var(--colorscale-resee-linear));
+
+    padding-bottom: 6px;
+
+    &.dismissable > div {
+      padding-right: 12rem;
+    }
+  }
+</style>
--- src/runtime/composables/use-resee-ux.ts
+++ src/runtime/composables/use-resee-ux.ts
@@ -1,3 +1,4 @@
+import { useCookie } from '#imports';
 import { reactive } from 'vue';
 
 
@@ -22,11 +23,26 @@ const Localization = reactive({
   },
 });
 
+type AppPreferences = {
+  dismissNotification?: string;
+};
+
+const msInYear = 31_536e6;
+
 
 /**
  * Provides the ability to manage runtime configuration of the ReSee UX
  * module.
  */
 export function useReseeUx() {
-  return { locale: Localization };
+  const preferences = useCookie<AppPreferences>('resee-app-preferences', {
+    default  : () => ({}),
+    secure   : true,
+    httpOnly : false,
+    sameSite : 'strict',
+    maxAge   : msInYear / 1000,
+    expires  : new Date(Date.now() + msInYear),
+  });
+
+  return { locale: Localization, preferences };
 }
