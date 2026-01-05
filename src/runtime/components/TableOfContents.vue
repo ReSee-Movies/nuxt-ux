1887ea9aa47f454b0a997001ff2abbbe93915311
Michael <mdeanjones@gmail.com>
2026-01-05 14:00:42 +0100

    feat: updated the TableOfContents component to mark the current anchor with and .active class name

--- src/runtime/components/TableOfContents.vue
+++ src/runtime/components/TableOfContents.vue
@@ -1,7 +1,18 @@
 <template>
   <ol v-if="props.toc?.length">
     <li v-for="entry of props.toc" :key="entry.slug">
-      <a :href="`#${ entry.slug }`" v-html="entry.text" />
+      <NuxtLink
+        v-slot  = "config"
+        :href   = "`#${ entry.slug }`"
+        :custom = "true"
+      >
+        <a
+          v-html = "entry.text"
+          :class = "{ active: mounted && areRoutesStrictEqual(config.route, route) }"
+          :href  = "config.href"
+        />
+      </NuxtLink>
+
       <TableOfContents v-if="entry.children.length" :toc="entry.children" />
     </li>
   </ol>
@@ -20,6 +31,20 @@
 
 
 <script setup lang="ts">
+  import { NuxtLink } from '#components';
+  import { useRoute } from '#imports';
+  import { onMounted, ref } from 'vue';
+  import { areRoutesStrictEqual } from '../utils/routing';
   import TableOfContents from './TableOfContents.vue';
-  const props = defineProps<UiTableOfContentsProps>();
+
+  const props   = defineProps<UiTableOfContentsProps>();
+  const route   = useRoute();
+  const mounted = ref(false);
+
+  // The `active` class name on links can only be applied client-side because
+  // hashes are not send to the server along with the rest of the URL. Not doing
+  // this would cause a hydration mismatch.
+  onMounted(() => {
+    mounted.value = true;
+  });
 </script>
