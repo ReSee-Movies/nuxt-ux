ae12afc46ade3a8a84fbf7fc3a0ad7a63ce66a8c
Michael <mdeanjones@gmail.com>
2026-03-25 13:47:10 +0100

    feat(ResponsiveSidebarContainer,ResponsiveSidebarLayout): introduce new components for layout of some primary content with a sidebar whose content collapses into a drawer below a certain breakpoint

---
+++ src/runtime/components/ResponsiveSidebarContainer.vue
@@ -0,0 +1,85 @@
+<template>
+  <ScrollPinnedContainer v-if="isColumn">
+    <slot :state="containerState" :is-drawer="isDrawer" :is-column="isColumn" />
+  </ScrollPinnedContainer>
+
+  <Drawer
+    v-else-if       = "isDrawer"
+    v-model:visible = "drawerVisible"
+    :position       = "props.drawerPosition"
+    :header         = "props.drawerHeader"
+    @click          = "handleDrawerClick"
+  >
+    <slot :state="containerState" :is-drawer="isDrawer" :is-column="isColumn" />
+  </Drawer>
+
+  <div v-else>
+    <slot :state="containerState" :is-drawer="isDrawer" :is-column="isColumn" />
+  </div>
+</template>
+
+
+<script lang="ts">
+  import type { DrawerProps } from './Drawer.vue';
+
+  export type ResponseSidebarContainerState = 'column' | 'drawer' | 'unknown';
+
+  export interface ResponsiveSidebarContainerProps {
+    drawerPosition? : DrawerProps['position'];
+    drawerHeader?   : DrawerProps['header'];
+    switchPoint?    : 'md' | 'lg';
+  }
+
+  export interface ResponsiveSidebarContainerEmits {
+    (e: 'stateChange', containerState: ResponseSidebarContainerState): void;
+  }
+</script>
+
+
+<script setup lang="ts">
+  import { useMounted } from '@vueuse/core';
+  import { computed, watch } from 'vue';
+  import { useReseeBreakpoints } from '../composables/use-resee-breakpoints';
+  import Drawer from './Drawer.vue';
+  import ScrollPinnedContainer from './ScrollPinnedContainer.vue';
+
+  const props = withDefaults(
+    defineProps<ResponsiveSidebarContainerProps>(),
+    {
+      drawerPosition : 'right',
+      drawerHeader   : undefined,
+      switchPoint    : 'lg',
+    },
+  );
+
+  const emits = defineEmits<ResponsiveSidebarContainerEmits>();
+
+  const drawerVisible      = defineModel<boolean | undefined>('drawerVisible', undefined);
+  const isComponentMounted = useMounted();
+  const isAtOrAboveSwitch  = useReseeBreakpoints().greaterOrEqual(props.switchPoint);
+
+  const containerState = computed<ResponseSidebarContainerState>(() => {
+    if (!isComponentMounted.value) {
+      return 'unknown';
+    }
+
+    return isAtOrAboveSwitch.value ? 'column' : 'drawer';
+  });
+
+  const isDrawer = computed(() => containerState.value === 'drawer');
+  const isColumn = computed(() => containerState.value === 'column');
+
+  watch(containerState, () => {
+    emits('stateChange', containerState.value);
+
+    if (!isDrawer.value) {
+      drawerVisible.value = undefined;
+    }
+  }, { immediate: true });
+
+  function handleDrawerClick(event: InputEvent) {
+    if (isDrawer.value && event.target instanceof HTMLElement && event.target.closest('a')) {
+      drawerVisible.value = false;
+    }
+  }
+</script>
---
+++ src/runtime/components/ResponsiveSidebarLayout.vue
@@ -0,0 +1,132 @@
+<template>
+  <div
+    :class="[
+      'sidebar-layout',
+      {
+        'collapse-md'  : props.switchPoint === 'md',
+        'collapse-lg'  : props.switchPoint === 'lg',
+        'orient-left'  : props.sidebarPosition === 'left',
+        'orient-right' : props.sidebarPosition === 'right',
+      },
+    ]"
+  >
+    <div>
+      <slot name="content" :sidebar-state="sidebarState" />
+    </div>
+
+    <div>
+      <ResponsiveSidebarContainer
+        v-model:drawer-visible = "drawerVisible"
+        :drawer-position       = "props.drawerPosition ?? props.sidebarPosition"
+        :drawer-header         = "props.drawerHeader"
+        :switch-point          = "props.switchPoint"
+        @state-change          = "handleSidebarStateChange"
+      >
+        <slot name="sidebar" :sidebar-state="sidebarState" />
+      </ResponsiveSidebarContainer>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts">
+  import type {
+    ResponsiveSidebarContainerEmits,
+    ResponsiveSidebarContainerProps,
+    ResponseSidebarContainerState,
+  } from './ResponsiveSidebarContainer.vue';
+
+  export interface ResponsiveSidebarLayoutProps extends ResponsiveSidebarContainerProps {
+    sidebarPosition?: 'left' | 'right';
+  }
+</script>
+
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import ResponsiveSidebarContainer from './ResponsiveSidebarContainer.vue';
+
+  const props = withDefaults(
+    defineProps<ResponsiveSidebarLayoutProps>(),
+    {
+      sidebarPosition : 'right',
+      switchPoint     : 'lg',
+    },
+  );
+
+  const emits = defineEmits<ResponsiveSidebarContainerEmits>();
+
+  const drawerVisible = defineModel<boolean | undefined>('drawerVisible', undefined);
+  const sidebarState  = ref<ResponseSidebarContainerState>('unknown');
+
+  function handleSidebarStateChange(state: ResponseSidebarContainerState) {
+    sidebarState.value = state;
+    emits('stateChange', state);
+  }
+</script>
+
+
+<style scoped>
+  @reference "tailwindcss";
+
+  @layer components {
+    .sidebar-layout {
+      --grid-cols-sidebar-right : 1fr 16rem;
+      --grid-cols-sidebar-left  : 16rem 1fr;
+      --sidebar-border          : solid 1px var(--color-global-background-accent);
+      --sidebar-border-gap      : --spacing(4);
+
+      display: grid;
+
+      &.collapse-lg {
+        @variant lg {
+          &.orient-right {
+            grid-template-columns: var(--grid-cols-sidebar-right);
+
+            & > div:first-child {
+              border-right  : var(--sidebar-border);
+              padding-right : var(--sidebar-border-gap);
+              margin-right  : var(--sidebar-border-gap);
+            }
+          }
+
+          &.orient-left {
+            grid-template-columns: var(--grid-cols-sidebar-left);
+
+            & > div:first-child {
+              order        : 2;
+              border-left  : var(--sidebar-border);
+              padding-left : var(--sidebar-border-gap);
+              margin-left  : var(--sidebar-border-gap);
+            }
+          }
+        }
+      }
+
+      &.collapse-md {
+        @variant md {
+          &.orient-right {
+            grid-template-columns: var(--grid-cols-sidebar-right);
+
+            & > div:first-child {
+              border-right  : var(--sidebar-border);
+              padding-right : var(--sidebar-border-gap);
+              margin-right  : var(--sidebar-border-gap);
+            }
+          }
+
+          &.orient-left {
+            grid-template-columns: var(--grid-cols-sidebar-left);
+
+            & > div:first-child {
+              order        : 2;
+              border-left  : var(--sidebar-border);
+              padding-left : var(--sidebar-border-gap);
+              margin-left  : var(--sidebar-border-gap);
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
