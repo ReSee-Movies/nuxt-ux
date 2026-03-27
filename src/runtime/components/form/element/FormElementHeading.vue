6ba88ef51350d5f3d72dc3cab102cfa53ed2b1c8
Michael <mdeanjones@gmail.com>
2026-03-27 11:19:43 +0100

    refactor: create FormElementHeading component, moving the markup out of FormFieldBuilder

---
+++ src/runtime/components/form/element/FormElementHeading.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="form-element-heading">
+    <h2 class="h2">
+      {{ props.heading }}
+    </h2>
+
+    <p v-if="props.subheading" class="p note">
+      {{ props.subheading }}
+    </p>
+  </div>
+</template>
+
+
+<script lang="ts">
+  export interface FormElementHeadingProps {
+    heading     : string;
+    subheading? : string;
+  }
+</script>
+
+
+<script setup lang="ts">
+  const props = withDefaults(
+    defineProps<FormElementHeadingProps>(),
+    {
+      subheading: undefined,
+    },
+  );
+</script>
