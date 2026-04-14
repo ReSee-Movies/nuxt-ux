c54d1f158179a0c438ac58036a7db7087b21c0b4
Michael <mdeanjones@gmail.com>
2026-04-14 13:40:21 +0200

    fix(build): configure nanocss, as needed, to disable its calc plugin which causes build-time warnings due to lack of support for certain usages of calc

--- src/utils/import-tailwind.ts
+++ src/utils/import-tailwind.ts
@@ -1,5 +1,6 @@
 import { addVitePlugin, useNuxt } from '@nuxt/kit';
 import type { Nuxt } from '@nuxt/schema';
+import type Chalk from 'chalk';
 
 
 const Builder    = '@nuxt/vite-builder';
@@ -40,5 +41,54 @@ export async function importTailwind(nuxt: Nuxt = useNuxt()) {
     if (!nuxt.options.postcss.plugins[PostPlugin]) {
       nuxt.options.postcss.plugins[PostPlugin] = {};
     }
+  }
+
+
+  if (nuxt.options.postcss?.plugins?.cssnano) {
+    const currentPreset = nuxt.options.postcss.plugins.preset;
+    let newPreset: ['default', Record<string, unknown>] | undefined = undefined;
+
+    if (!currentPreset || (typeof currentPreset === 'string' && currentPreset === 'default')) {
+      newPreset = ['default', { calc: false }];
+    }
+    else if (Array.isArray(currentPreset) && currentPreset[0] === 'default') {
+      newPreset = ['default', { ...currentPreset[1], calc: false }];
+    }
+    else {
+      let chalk: Chalk.Chalk;
+
+      try {
+        ({ default: chalk } = await import('chalk'));
+      }
+      catch {
+        chalk = {
+          magenta : (text: string) => text,
+          green   : (text: string) => text,
+        } as Chalk.Chalk;
+      }
+
+      console.warn(
+        chalk.yellow(
+          '[module @resee-movies/nuxt-ux:import-tailwind]\n\n'
+          + 'It appears that NanoCSS is enabled in your build, but done so in a way that prevents this module\n'
+          + 'from accessing its preset configuration.\n\n'
+          + 'The NanoCSS "calc" plugin will emit a warning when it encounters certain syntax - specifically calc\n'
+          + 'functions within CSS color module level 5 grammar - which this module heavily relies on.\n\n'
+          + 'These warnings have no effect on the resulting output, but can cause a noisy console during builds.\n'
+          + 'To disable, within your "nuxt.config.ts", add:\n',
+        )
+        + '\n{'
+        + `\n  ${ chalk.magenta('postcss') }: {`
+        + `\n    ${ chalk.magenta('plugins') }: {`
+        + `\n      ${ chalk.magenta('nanocss') }: {`
+        + `\n        ${ chalk.magenta('preset') }: [${ chalk.green('"default"') }, { ${ chalk.magenta('calc') }: false }],`
+        + '\n      },'
+        + '\n    },'
+        + '\n  },'
+        + '\n}',
+      );
+    }
+
+    nuxt.options.postcss.plugins.cssnano.preset = newPreset;
   }
 }
